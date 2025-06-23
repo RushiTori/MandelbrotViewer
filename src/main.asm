a7d0d98ad2ebde75865f5c9efb0dae67a4c17da0
RushiTori <Arcturusarc09@gmail.com>
2025-06-23 15:14:53 +0200

    added a vectorized implementation, it's big spaghetti
    I also added a kill switch in case the vectorized and scalar versions differ and they do
    
    Even after checking my maths for hours and debugging with both GDB, an external calculator and C program doing the same math I can't find why the vectorized version doesn't work

--- src/main.asm
+++ src/main.asm
@@ -12,7 +12,7 @@ var(static, float_t, mouse_radius, 50.0e0)
 var(static, double_t, neg_d, 0x8000_0000_0000_0000)
 var(static, double_t, x_step_d, 0.0025)
 var(static, double_t, y_step_d, 0.0025)
-var(static, double_t, inf_d, 1.0e11)
+var(static, double_t, inf_d, 100.0)
 var(static, double_t, view_size_d, 4.0)
 var(static, double_t, view_correction_d, 2.0)
 var(static, double_t, one_over_max_iter_d, 0.005)
@@ -63,6 +63,61 @@ func(static, get_mandelbrot_d)
 	.end:
 	ret
 
+; uint64_t get_mandelbrot_v(double a, double b, double inf, uint64_t iter);
+func(static, get_mandelbrot_v)
+	; cmp rdi, 0
+	; je  .end
+
+	; ca: double = a
+	; cb: double = b
+
+	; a: double = ca
+	; b: double = cb
+
+	; loop_start
+
+		; na: double = a * a - b * b
+		; nb: double = 2 * b * b
+
+		; a: double = na + ca
+		; b: double = nb + cb
+
+		; z: double = a + b
+
+	vshufps xmm0, xmm0, xmm1, 0b01_00_01_00 ;                        {ca, cb}
+	movaps xmm3, xmm0 ;                                              {a, b}
+
+	mov      rax,  2
+	cvtsi2sd xmm1, rax ; double(2)
+	xor      rax,  rax
+
+	.loop_:
+		xorps xmm4, xmm4
+		mulpd xmm3, xmm3 ;                                           {a*a, b*b}
+		vshufps xmm4, xmm3, xmm3, 0b11_10 ; double(b*b)
+		subsd xmm3, xmm4 ; double(a*a-b*b)
+		mulsd xmm4, xmm1 ; double(b*b*2)
+		vshufps xmm3, xmm3, xmm4, 0b01_00_01_00 ;                    {a*a-b*b, b*b*2}
+		addpd xmm3, xmm0 ;                                           {a*a-b*b+ca, b*b*2+cb}
+		vshufps xmm4, xmm3, xmm3, 0b01_00_11_10 ;                    {b*b*2+cb, a*a-b*b+ca}
+		addsd xmm3, xmm4 ;                                           {a*a-b*b+ca+b*b*2+cb, b*b*2+cb}
+
+		xorps  xmm5, xmm5             ;                              {0, 0}
+		movq   xmm5, double_p [neg_d] ; neg_flag_d
+		xorpd  xmm5, xmm3             ; double(-(a*a-b*b+ca+b*b*2+cb))
+		maxsd  xmm5, xmm3             ; double(max(double(-(a*a-b*b+ca+b*b*2+cb)), double((a*a-b*b+ca+b*b*2+cb))))
+		comisd xmm5, xmm2             ; check with inf_d
+		jae    .end
+
+		vshufps xmm3, xmm4, xmm3, 0b11_10_11_10 ;                    {a*a-b*b+ca, b*b*2+cb}
+
+		inc rax
+		cmp rax, rdi
+		jl  .loop_
+
+	.end:
+	ret
+
 func(static, update_game)
 	;sub rsp, 8
 	
@@ -88,6 +143,39 @@ func(static, render_game)
 	.loop_y:
 		xor r13, r13
 		.loop_x:
+			xorps xmm0, xmm0
+			xorps xmm1, xmm1
+			xorps xmm2, xmm2
+			xorps xmm3, xmm3
+			xorps xmm4, xmm4
+			xorps xmm5, xmm5
+			xorps xmm6, xmm6
+			xorps xmm7, xmm7
+
+			cvtsi2sd xmm0, r13
+			mulsd    xmm0, double_p [x_step_d]
+			mulsd    xmm0, double_p [view_size_d]
+			subsd    xmm0, double_p [view_correction_d]
+
+			cvtsi2sd xmm1, r12
+			mulsd    xmm1, double_p [y_step_d]
+			mulsd    xmm1, double_p [view_size_d]
+			subsd    xmm1, double_p [view_correction_d]
+
+			movq xmm2, double_p [inf_d]
+			mov  rdi,  MAX_ITER
+			call get_mandelbrot_v
+			mov  rcx,  rax
+
+			xorps xmm0, xmm0
+			xorps xmm1, xmm1
+			xorps xmm2, xmm2
+			xorps xmm3, xmm3
+			xorps xmm4, xmm4
+			xorps xmm5, xmm5
+			xorps xmm6, xmm6
+			xorps xmm7, xmm7
+
 			cvtsi2sd xmm0, r13
 			mulsd    xmm0, double_p [x_step_d]
 			mulsd    xmm0, double_p [view_size_d]
@@ -101,6 +189,9 @@ func(static, render_game)
 			movq xmm2, double_p [inf_d]
 			mov  rdi,  MAX_ITER
 			call get_mandelbrot_d
+
+			cmp rax, rcx
+			jne kill_loop
 
 			cmp      rax,  MAX_ITER
 			jne      .skip_zero
@@ -138,6 +229,9 @@ func(static, render_game)
 	
 	add rsp, 8
 	ret
+
+kill_loop:
+	jmp kill_loop
 
 func(global, _start)
 	mov  rdi, uint64_p [rsp]           ; argc
