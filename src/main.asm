f78710805e19a970b2e20fb772c0313d59279bc9
RushiTori <Arcturusarc09@gmail.com>
2025-06-23 10:11:17 +0200

    now using doubles instead of floats to prepare for the demand in precision

--- src/main.asm
+++ src/main.asm
@@ -3,78 +3,72 @@ default rel
 
 %include "main.inc"
 
-MAX_ITER equ 100
+MAX_ITER equ 200
 
 section      .rodata
 
 var(static, float_t, mouse_radius, 50.0e0)
-var(static, float_t, neg_f, 0x80_00_00_00)
 
-var(static, float_t, x_step_f, 0.0025)
-var(static, float_t, y_step_f, 0.0025)
-var(static, float_t, inf_f, 32.0)
-var(static, float_t, view_size_f, 4.0)
-var(static, float_t, view_correction_f, 2.0)
-var(static, float_t, one_over_max_iter_f, 0.01)
-var(static, float_t, uint8max_f, 255.0)
-
-section      .bss
-
-res(static,  vector2_t, mouse_pos_v)
+var(static, double_t, neg_d, 0x8000_0000_0000_0000)
+var(static, double_t, x_step_d, 0.0025)
+var(static, double_t, y_step_d, 0.0025)
+var(static, double_t, inf_d, 1.0e11)
+var(static, double_t, view_size_d, 4.0)
+var(static, double_t, view_correction_d, 2.0)
+var(static, double_t, one_over_max_iter_d, 0.005)
+var(static, double_t, uint8max_d, 255.0)
 
 section      .text
 
-; uint64_t get_mandelbrot(float a, float b, float inf, uint64_t iter);
-func(static, get_mandelbrot)
+; uint64_t get_mandelbrot_d(double a, double b, double inf, uint64_t iter);
+func(static, get_mandelbrot_d)
 	cmp rdi, 0
 	je  .end
 
-	movss xmm3, xmm0
-	movss xmm4, xmm1
+	movsd xmm3, xmm0
+	movsd xmm4, xmm1
 
 	mov      rax,  2
-	cvtsi2ss xmm5, rax
+	cvtsi2sd xmm5, rax
 	xor      rax,  rax
 
 	.loop_:
-		movss xmm6, xmm3
-		mulss xmm6, xmm4
-		mulss xmm6, xmm5
+		movsd xmm6, xmm3
+		mulsd xmm6, xmm4
+		mulsd xmm6, xmm5
 
-		mulss xmm3, xmm3
-		mulss xmm4, xmm4
+		mulsd xmm3, xmm3
+		mulsd xmm4, xmm4
 
-		subss xmm3, xmm4
-		movss xmm4, xmm6
+		subsd xmm3, xmm4
+		movsd xmm4, xmm6
 
-		addss xmm3, xmm0
-		addss xmm4, xmm1
+		addsd xmm3, xmm0
+		addsd xmm4, xmm1
 
-		movss xmm6, xmm3
-		addss xmm6, xmm4
+		movsd xmm6, xmm3
+		addsd xmm6, xmm4
 
-		movd  xmm7, float_p [neg_f]
+		movq  xmm7, double_p [neg_d]
 		xorps xmm7, xmm6
-		maxss xmm6, xmm7
+		maxsd xmm6, xmm7
 
-		comiss xmm6, xmm2
+		comisd xmm6, xmm2
 		jae    .end
 
 		inc rax
 		cmp rax, rdi
-		jle .loop_
+		jl  .loop_
 	
 	.end:
 	ret
 
 func(static, update_game)
-	sub rsp, 8
+	;sub rsp, 8
 	
 	; Update code goes here
-	call GetMousePosition
-	movq vector2_p [mouse_pos_v], xmm0
 
-	add rsp, 8
+	;add rsp, 8
 	ret
 
 func(static, render_game)
@@ -94,32 +88,34 @@ func(static, render_game)
 	.loop_y:
 		xor r13, r13
 		.loop_x:
-			cvtsi2ss xmm0, r13
-			mulss    xmm0, float_p [x_step_f]
-			mulss    xmm0, float_p [view_size_f]
-			subss    xmm0, float_p [view_correction_f]
+			cvtsi2sd xmm0, r13
+			mulsd    xmm0, double_p [x_step_d]
+			mulsd    xmm0, double_p [view_size_d]
+			subsd    xmm0, double_p [view_correction_d]
 
-			cvtsi2ss xmm1, r12
-			mulss    xmm1, float_p [y_step_f]
-			mulss    xmm1, float_p [view_size_f]
-			subss    xmm1, float_p [view_correction_f]
+			cvtsi2sd xmm1, r12
+			mulsd    xmm1, double_p [y_step_d]
+			mulsd    xmm1, double_p [view_size_d]
+			subsd    xmm1, double_p [view_correction_d]
 
-			movd xmm2, float_p [inf_f]
+			movq xmm2, double_p [inf_d]
+			mov  rdi,  MAX_ITER
+			call get_mandelbrot_d
 
-			mov rdi, MAX_ITER
+			cmp      rax,  MAX_ITER
+			jne      .skip_zero
+			xor      rax,  rax
+			.skip_zero:
+			cvtsi2sd xmm0, rax
 
-			call get_mandelbrot
+			mulsd    xmm0, double_p [one_over_max_iter_d]
+			mulsd    xmm0, double_p [uint8max_d]
+			cvtsd2si rax,  xmm0
+			mov      rdx,  0x01_01_01_01
+			mul      edx
+			mov      edx,  eax
 
-			cvtsi2ss xmm0, rax
-			mulss    xmm0, float_p [one_over_max_iter_f]
-			mulss    xmm0, float_p [uint8max_f]
-			cvtss2si rax,  xmm0
-
-			mov rdx, 0x01_01_01_01
-			mul edx
-			mov edx, eax
-			or  edx, A_MASK
-
+			or   edx, A_MASK
 			mov  rdi, r13
 			mov  rsi, r12
 			call DrawPixel
