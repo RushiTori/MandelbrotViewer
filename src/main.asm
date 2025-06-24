b9736de92e07a8a8c38fd8692e5beddc36cac0dd
RushiTori <Arcturusarc09@gmail.com>
2025-06-24 15:02:53 +0200

    moved the mandelbrot code to its own file to make it easier to add new functionality to it later
    reduced the number of MAX iteration from 200 to 100

--- src/main.asm
+++ src/main.asm
@@ -1,191 +1,15 @@
-bits    64
-default rel
+bits         64
+default      rel
 
 %include "main.inc"
 
-MAX_ITER equ 200
-
-section      .rodata
-
-string(static, debug_fmt, "%g, {%g, %g}", 0xa, 0)
-
-var(static, double_t, one_d, 1.0)
-
-var(static, double_t, neg_d, 0x8000_0000_0000_0000)
-
-var(static, double_t, inf_d, 100.0)
-var(static, double_t, one_over_max_iter_d, 0.005)
-var(static, double_t, uint8max_d, 255.0)
-
-var(static, float_t, zoom_factor_f, 0.97)
-var(static, uint64_t, pan_speed_x_u, 4)
-var(static, uint64_t, pan_speed_y_u, 4)
-
-section      .data
-
-var(static,  uint64_t, width_u, 400)
-var(static,  uint64_t, height_u, 400)
-
-var(static,  uint64_t, half_width_u, 200)
-var(static,  uint64_t, half_height_u, 200)
-
-var(static,  double_t,  one_over_width_d, 0.0025)
-var(static,  double_t,  one_over_height_d, 0.0025)
-
-var(static, double_t, world_size_d, 4.0)
-
-var(static, double_t, view_scale_x_d, 0.01)
-var(static, double_t, view_scale_y_d, 0.01)
-
-var(static, double_t, view_offset_x_d, 0.0)
-var(static, double_t, view_offset_y_d, 0.0)
-
 section      .text
 
-; typedef struct {double x, y} VecD2;
-; VecD2 pixel_to_world(uint64_t x, uint64_t y);
-func(static, pixel_to_world)
-	sub      rdi,  uint64_p [half_width_u]
-	cvtsi2sd xmm0, rdi
-	mulsd    xmm0, double_p [view_scale_x_d]
-	addsd    xmm0, double_p [view_offset_x_d]
-
-	sub      rsi,  uint64_p [half_height_u]
-	cvtsi2sd xmm1, rsi
-	mulsd    xmm1, double_p [view_scale_y_d]
-	addsd    xmm1, double_p [view_offset_y_d]
-	ret
-
-; uint64_t mandelbrot(double a, double b, double inf, uint64_t iter);
-func(static, mandelbrot)
-	cmp rdi, 0
-	je  .end
-
-	vshufps xmm0, xmm0, xmm1, 0b01_00_01_00
-	movaps xmm3, xmm0
-
-	mov      rax,  2
-	cvtsi2sd xmm1, rax
-	xor      rax,  rax
-
-	.loop_:
-		vshufps xmm4, xmm3, xmm3, 0b11_10
-		mulsd xmm4, xmm3
-		mulpd xmm3, xmm3
-		vshufps xmm5, xmm3, xmm3, 0b11_10
-		subsd xmm3, xmm5
-		mulsd xmm4, xmm1
-		vshufps xmm3, xmm3, xmm4, 0b01_00_01_00
-		addpd xmm3, xmm0
-		vshufps xmm4, xmm3, xmm3, 0b01_00_11_10
-		addsd xmm3, xmm4
-
-		xorps  xmm5, xmm5
-		movq   xmm5, double_p [neg_d]
-		xorpd  xmm5, xmm3
-		maxsd  xmm5, xmm3
-		comisd xmm5, xmm2
-		jae    .end
-
-		vshufps xmm3, xmm4, xmm3, 0b11_10_11_10
-
-		inc rax
-		cmp rax, rdi
-		jl  .loop_
-
-	.end:
-	ret
-
 func(static, update_game)
-	sub rsp, 8
-	
-	; Update code goes here
-
-	; Updating window size infos
-		call GetScreenWidth
-		mov  uint64_p [width_u], rax
-
-		movq     xmm0,                        double_p [one_d]
-		cvtsi2sd xmm1,                        rax
-		divsd    xmm0,                        xmm1
-		movq     double_p [one_over_width_d], xmm0
-
-		shr rax,                     1
-		mov uint64_p [half_width_u], rax
-
-		call GetScreenHeight
-		mov  uint64_p [height_u], rax
-
-		movq     xmm0,                         double_p [one_d]
-		cvtsi2sd xmm1,                         rax
-		divsd    xmm0,                         xmm1
-		movq     double_p [one_over_height_d], xmm0
-
-		shr rax,                      1
-		mov uint64_p [half_height_u], rax
-
-	; Handle zoom
-		call  GetMouseWheelMove
-		xorps xmm1, xmm1
-
-		ucomiss  xmm0,                    xmm1
-		je       .skip_zoom
-		xorps    xmm1,                    xmm1
-		mulss    xmm0,                    float_p [zoom_factor_f]
-		cvtss2sd xmm0,                    xmm0
-		movq     xmm1,                    double_p [world_size_d]
-		mulsd    xmm1,                    xmm0
-		movq     xmm0,                    double_p [neg_d]
-		orpd     xmm1,                    xmm0
-		xorpd    xmm1,                    xmm0
-		movq     double_p [world_size_d], xmm1
-		.skip_zoom:
-
-
-		movq xmm1, double_p [world_size_d]
-
-		movq  xmm0,                      double_p [one_over_width_d]
-		mulsd xmm0,                      xmm1
-		movq  double_p [view_scale_x_d], xmm0
-
-		movq  xmm0,                      double_p [one_over_height_d]
-		mulsd xmm0,                      xmm1
-		movq  double_p [view_scale_y_d], xmm0
-
-	; Handle pan
-		mov  rdi, MOUSE_BUTTON_LEFT
-		call IsMouseButtonDown
-		cmp  al,  false
-		je   .skip_pan
-
-		call     GetMouseDelta
-		movss    xmm1, xmm0
-		shufps xmm0, xmm0, 1
-		cvtss2sd xmm0, xmm0
-		cvtss2sd xmm1, xmm1
-
-		mulsd xmm1,                       double_p [view_scale_x_d]
-		movq  xmm2,                       double_p [view_offset_x_d]
-		subsd xmm2,                       xmm1
-		movq  double_p [view_offset_x_d], xmm2
-
-		mulsd xmm0,                       double_p [view_scale_y_d]
-		movq  xmm2,                       double_p [view_offset_y_d]
-		subsd xmm2,                       xmm0
-		movq  double_p [view_offset_y_d], xmm2
-
-		.skip_pan:
-
-	add rsp, 8
-	ret
+	jmp update_mandelbrot
 
 func(static, render_game)
 	sub rsp, 8
-
-	push r12
-	push r13
-	push r14
-	push r15
 
 	call BeginDrawing
 
@@ -194,56 +18,13 @@ func(static, render_game)
 
 	; Render code goes here
 
-	mov r14, uint64_p [height_u]
-	mov r15, uint64_p [width_u]
-
-	xor r12, r12
-	.loop_y:
-		xor r13, r13
-		.loop_x:
-			mov  rdi, r13
-			mov  rsi, r12
-			call pixel_to_world
-
-			movq xmm2, double_p [inf_d]
-			mov  rdi,  MAX_ITER
-			call mandelbrot
-
-			cmp      rax,  MAX_ITER
-			jne      .skip_zero
-			xor      rax,  rax
-			.skip_zero:
-			cvtsi2sd xmm0, rax
-
-			mulsd    xmm0, double_p [one_over_max_iter_d]
-			mulsd    xmm0, double_p [uint8max_d]
-			cvtsd2si rax,  xmm0
-			mov      rdx,  0x01_01_01_01
-			mul      edx
-			mov      edx,  eax
-
-			or   edx, A_MASK
-			mov  rdi, r13
-			mov  rsi, r12
-			call DrawPixel
-
-			inc r13
-			cmp r13, r15
-			jne .loop_x
-		inc r12
-		cmp r12, r14
-		jne .loop_y
+	call render_mandelbrot
 
 	xor  rdi, rdi
 	xor  rsi, rsi
 	call DrawFPS
 
 	call EndDrawing
-
-	pop r15
-	pop r14
-	pop r13
-	pop r12
 	
 	add rsp, 8
 	ret
